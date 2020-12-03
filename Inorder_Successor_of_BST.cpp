3a4c8c7bed69cadf804007cd3036255237d8502b
JatinK <jatin998kk@gmail.com>
2020-12-04 03:47:56 +0530

    Create Inorder_Successor_of_BST.cpp

---
+++ Inorder_Successor_of_BST.cpp
@@ -0,0 +1,55 @@
+
+/*The structure of Node
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// returns the inorder successor of the Node x in BST (rooted at 'root')
+
+Node *getleftmost(Node *x)        // returns the left most node 
+ {
+     if(x->left==NULL)
+      return x;
+     else
+      getleftmost(x->left);
+ }
+
+Node * inOrderSuccessor(Node *root, Node *x)
+{
+    //Your code here
+    if(x == NULL)  return NULL;
+    
+    if(x->right != NULL) // if right child exists go right
+      {
+         return getleftmost(x->right);          // return min in the right most subtree
+      }
+      
+      Node *succ=NULL;
+      while(root != NULL)
+       {
+           if(x->data < root->data) 
+            {
+                // ans exists in left subtree
+                succ =root;
+                root =root->left;
+            }
+           else if(x->data > root->data)
+            {
+                // ans exists in right subtree
+                root=root->right;
+            }
+            else
+             break;
+       }
+      
+      return succ;
+}
